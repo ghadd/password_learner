bd7ea699b0fb96339a3b414ec8e2891d5002da55
Dan <danyatimachov@gmail.com>
2021-01-24 04:31:48 +0200

    Setting up setup.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from pwdgen.password import Password
 from pwdgen import PasswordGenerator as PGen
-from parser import parser
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Learn passwords!")
+parser.add_argument("--new", action="store_true", dest="create_new")
+parser.add_argument("--allowed-chars", dest="allowed_chars", default=None)
+parser.add_argument(
+    "-e", "--exclude-ambigous", dest="exclude_ambigous_chars", action="store_true"
+)
+parser.add_argument("-l", "--length", dest="length", default=16)
 
 
 if __name__ == "__main__":
@@ -15,8 +24,11 @@ if __name__ == "__main__":
         except IOError:
             pass
 
-    gen = PGen(args.length, allowed_chars=args.allowed_chars,
-               exclude_ambigous_chars=args.exclude_ambigous_chars)
+    gen = PGen(
+        args.length,
+        allowed_chars=args.allowed_chars,
+        exclude_ambigous_chars=args.exclude_ambigous_chars,
+    )
     pwd = gen.generate()
 
     pwd.learn()
