2fd90afbcdb61aebbedec5629e9278496edce175
Dan <danyatimachov@gmail.com>
2021-01-24 10:33:54 +0200

    Transferring parser.py contents directly to the main script.

--- parser.py
+++
@@ -1,8 +0,0 @@
-from argparse import ArgumentParser
-
-parser = ArgumentParser(description="Learn passwords!")
-parser.add_argument("--new", action="store_true", dest="create_new")
-parser.add_argument("--allowed-chars", dest="allowed_chars", default=None)
-parser.add_argument("-e", "--exclude-ambigous",
-                    dest="exclude_ambigous_chars", action="store_true")
-parser.add_argument("-l", "--length", dest="length", default=16)
